2b6e5e8cf0542dfef34096427bf8ed3f3e36d1a2
Bianca Francis <kadu_300@live.com>
2023-04-20 10:42:33 -0300

    Update Postagem.java
    Atualização

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -4,57 +4,85 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-@Entity
-@Table(name = "tb_postagem")
+@Entity /* gerar tabela banco de dados */
+@Table(name = "tb_postagens") /* create table tb_postegens */
 public class Postagem {
-
+	
+//varias postagens com tipo long
+	// indicar id e mostrar tipo
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotBlank(message = "O título é obrigatório!")
-	@Size(min = 5, max = 100, message ="O título deve ter no mínimo 5 e no máximo 100 caracteres.")
+	@NotBlank(message = "O atributo título é obrigatório!")
+	@Size(min = 5, max = 100, message = "O atributo título deve ter no minimo 05 e no máximo 100 caracteres")
 	private String titulo;
 	
-	@NotBlank(message = "O texto é obrigatório!")
-	@Size(min = 10, max = 1000, message ="O texto deve ter no mínimo 10 e no máximo 1000 caracteres.")
+	@NotBlank(message = "O atributo texto é obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo texto deve ter no minimo 10 e no máximo 1000 caracteres")
 	private String texto;
 	
+	//pegar data e hora do sistema e mantem atualizado
 	@UpdateTimestamp
 	private LocalDateTime data;
+
+	/* Relacionamento */
+	@JsonIgnoreProperties("postagem")
+	@ManyToOne
+	private Tema tema;
+	
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getTexto() {
 		return texto;
 	}
+
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
+
 	public LocalDateTime getData() {
 		return data;
 	}
+
 	public void setData(LocalDateTime data) {
 		this.data = data;
 	}
+
+	/* Criar os Métodos Get e Set do Objeto Tema*/
 	
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 	
 	
 }
